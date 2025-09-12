fb6a704e8f47db5d371040cd671ce77634554d58
victor braylovsky <vvb@bk.ru>
2025-09-12 16:27:22 +0200

    psy-natalia example

---
+++ psy-natalia/_next/static/T4lP2BW7lrallPFLusaMY/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/ae5d0382792ac1c3.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/psy-natalia//_next/:path+",
+        "destination": "/psy-natalia/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
